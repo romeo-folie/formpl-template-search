99d46828a3f80c817f9694c09f1ffafec67dc12c
Romeo Folie <romeofolie1@gmail.com>
2021-06-15 19:10:47 +0000

    :)

--- src/components/with-spinner/with-spinner.component.tsx
+++ src/components/with-spinner/with-spinner.component.tsx
@@ -3,23 +3,22 @@ import {SpinnerOverlay, SpinnerContainer} from "./with-spinner.styles";
 import {useSelector} from "react-redux";
 import {TemplateState} from "../../redux/templates/template.reducer";
 
-const WithSpinner =
-  <P extends object>(WrappedComponent: React.FC<P>) => {
-    const isLoading = useSelector<TemplateState, TemplateState["isFetching"]>(
-      (state) => state.isFetching
-    );
+const WithSpinner = <P extends object>(WrappedComponent: React.FC<P>) => {
+  const isLoading = useSelector<TemplateState, TemplateState["isFetching"]>(
+    (state) => state.isFetching
+  );
 
-    const Spinner = ({...otherProps}) => {
-      return isLoading ? (
-        <SpinnerOverlay>
-          <SpinnerContainer />
-        </SpinnerOverlay>
-      ) : (
-        <WrappedComponent {...(otherProps as P)} />
-      );
-    };
-    
-    return Spinner;
-  }
-  
+  const Spinner = ({...props}: P) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...props} />
+    );
+  };
+
+  return Spinner;
+};
+
 export default WithSpinner;
